02a43e6ef2fc747659fec90b37a152acc6ca40ed
kur <mikio@kur.jp>
2013-09-11 18:51:33 +0900

    no message

---
+++ app/Controller/EventsController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class EventsController extends AppController {
+
+	public $uses = array();
+
+	public function  index(){
+		
+	}
+	// list
+	public function  viewall(){
+		
+	}
+	public function  create(){
+		
+	}
+	public function  view($eventId = 0){
+		
+	}
+	public function  change($eventId = 0){
+	
+	}	
+}
