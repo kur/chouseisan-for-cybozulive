71d0f726be48a7cf06a9ed2d8ee3f0f3d1b16122
kur <mikio@kur.jp>
2013-09-24 22:14:33 +0900

    多分コイツしばらく使わない

--- app/Controller/GroupsController.php
+++ app/Controller/GroupsController.php
@@ -5,18 +5,18 @@ class GroupsController extends AppController {
 
 	public $uses = array("Event", "Group", "User", "Registration", "Candidate");
 
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->Group->create();
-			if ($this->Group->save($this->request->data)) {
-				$this->Session->setFlash('グループが追加されました');
-				$this->redirect(array('controller' => 'groups',
-						'action' => 'view'));
-			} else {
-				$this->Session->setFlash('選択肢を追加できませんでした');
-			}
-		}
-	}
+	// 	public function add() {
+	// 		if ($this->request->is('post')) {
+	// 			$this->Group->create();
+	// 			if ($this->Group->save($this->request->data)) {
+	// 				$this->Session->setFlash('グループが追加されました');
+	// 				$this->redirect(array('controller' => 'groups',
+	// 						'action' => 'view'));
+	// 			} else {
+	// 				$this->Session->setFlash('選択肢を追加できませんでした');
+	// 			}
+	// 		}
+	// 	}
 
 	public function view() {
 		$groups = $this->Group->find('all');
